56b2a0456c55c03f7cd6dc2d118f5d3bbc544099
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-10-03 10:28:41 +0900

    todoリストを作成、表示できるようにしました

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string; //タイトル
+    id: number; //ID
+    status: number; //4つのボード
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      status: 1,
+    };
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='m-10'>
+      <h1 className='text-center text-4xl py-10'>TODO管理アプリ</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" onChange={(e) => handleChange(e)} className='input input-bordered' />
+          <input type="submit" value="+" className='btn'/>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>・{todo.inputValue}</li>
+        ))}
+      </ul>
     </div>
   );
 }
